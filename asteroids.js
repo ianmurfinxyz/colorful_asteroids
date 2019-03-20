dabf76fed0e631a2c70dff16b5b9d14410eebbad
snail-bait <imurfin.1@gmail.com>
2019-03-20 18:31:22 +0000

    updated github url

--- asteroids.js
+++ asteroids.js
@@ -1,6 +1,6 @@
 //==============================================================================
 // completion-date: 19.03.2019
-// author: Ian Murfin (https://github.com/snail-bait)
+// author: Ian Murfin (https://github.com/zombie-snail)
 //==============================================================================
 
 (function(){
